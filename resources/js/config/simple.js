fd4771a436dec9ec28ecc4592757294213d59c5b
Jineshfrancisco <jineshfrancis@hotmail.com>
2020-05-14 04:41:04 +0530

    rsource file updated

--- resources/js/config/simple.js
+++ resources/js/config/simple.js
@@ -1,14 +1,14 @@
 // navbar Active class
 let url = window.location.pathname + window.location.search;
 if (url === '/home') {
-	document.querySelector('.home').classList.add('active');
+	document.querySelector('.home').classList.add('is-active');
 } else if (url === '/contact') {
-	document.querySelector('.contact').classList.add('active');
+	document.querySelector('.contact').classList.add('is-active');
 } else if (url === '/services') {
-	document.querySelector('.services').classList.add('active');
+	document.querySelector('.services').classList.add('is-active');
 } else if (url === '/about') {
-	document.querySelector('.about').classList.add('active');
+	document.querySelector('.about').classList.add('is-active');
 } else {
 	// for "if(url === /')"
-	document.querySelector('.home').classList.add('active');
+	document.querySelector('.home').classList.add('is-active');
 }
